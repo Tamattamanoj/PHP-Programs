ed2f523099a5d60710a847b57512959530d4d6dc
Tamattamanoj <107041232+Tamattamanoj@users.noreply.github.com>
2022-06-08 12:53:32 +0530

    New changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 
 	public static void main(string[] args) {
-	// Print "Hello world" to the terminal window.
+	// Print "Hello world" to the terminal window. New changes
 	system.out.println("Hello, World");
 	}
 	}
